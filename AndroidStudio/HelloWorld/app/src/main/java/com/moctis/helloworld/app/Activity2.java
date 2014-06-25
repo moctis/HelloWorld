b020357f42ba46bb2baa31b7c9d94a78f4348bcb
sitthidate nanuea <sitthidate@moctis.com>
2014-06-26 02:38:25 +0700

    Reformat code

--- AndroidStudio/HelloWorld/app/src/main/java/com/moctis/helloworld/app/Activity2.java
+++ AndroidStudio/HelloWorld/app/src/main/java/com/moctis/helloworld/app/Activity2.java
@@ -23,12 +23,14 @@ public class Activity2 extends ActionBarActivity {
     }
 
     @Override
+
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        return (id == R.id.action_settings) || super.onOptionsItemSelected(item);
+        return
+                (id == R.id.action_settings) || super.onOptionsItemSelected(item);
     }
 
 }
